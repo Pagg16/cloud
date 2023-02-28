5d43e009f5e0bd5ba0edd58e07d157d8806edbc6
Pavel Kulebin <pagg16pagg@gmail.com>
2023-02-28 21:04:26 +0300

    client + server

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+
+const authRouter = require("./routes/auth.routes");
+
+const app = express();
+const PORT = config.get("serverPort");
+
+app.use(express.json());
+app.use("/api/auth", authRouter);
+
+const start = async () => {
+  try {
+    await mongoose
+      .connect(config.get("dbUrl"))
+      .then(console.log("Connect mongoose"));
+
+    app.listen(PORT, () => {
+      console.log("Server started on ", PORT);
+    });
+  } catch (e) {
+    console.log("Error type ", e);
+  }
+};
+
+start();
