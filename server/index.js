89a95d978f05251ea0d38416eb243886e855b032
Pavel Kulebin <pagg16pagg@gmail.com>
2023-03-01 15:20:06 +0300

    client+registration, server+cors

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
-
+const corsMiddlweare = require("./middleweare/cors.middleweare");
 const authRouter = require("./routes/auth.routes");
 
 const app = express();
 const PORT = config.get("serverPort");
 
+app.use(corsMiddlweare);
 app.use(express.json());
 app.use("/api/auth", authRouter);
 
