531f0a38bb9d3238d3ef85990f1698c8a15bc962
Pavel Kulebin <pagg16pagg@gmail.com>
2023-03-07 21:02:57 +0300

    client+animation

--- client/src/components/disk/fileList/Filelist.jsx
+++ client/src/components/disk/fileList/Filelist.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import File from "./file/File";
 
 import "./filelist.less";
@@ -14,9 +15,18 @@ const FileList = () => {
         <div className="filelist__data">Дата</div>
         <div className="filelist__size">Размер</div>
       </div>
-      {files.map((file) => {
-        return <File file={file} key={file._id} />;
-      })}
+      <TransitionGroup>
+        {files.map((file) => (
+          <CSSTransition
+            key={file._id}
+            timeout={500}
+            classNames={"file"}
+            exit={false}
+          >
+            <File file={file} key={file._id} />;
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
     </div>
   );
 };
--- client/src/components/disk/fileList/file/File.jsx
+++ client/src/components/disk/fileList/file/File.jsx
@@ -5,6 +5,7 @@ import fileLogo from "../../../../assets/img/file.svg";
 import { useDispatch, useSelector } from "react-redux";
 import { pushDirStack, setCurrentDir } from "../../../../reduser/fileReduser";
 import { deleteFile, downloadfile } from "../../../../actions/file";
+import sizeFormat from "../../../../utils//sizeFormat";
 
 const File = ({ file }) => {
   const dispatch = useDispatch();
@@ -36,7 +37,7 @@ const File = ({ file }) => {
       />
       <div className="file__name">{file.name}</div>
       <div className="file__date">{file.date.slice(0, 10)}</div>
-      <div className="file__size">{file.size}</div>
+      <div className="file__size">{sizeFormat(file.size)}</div>
       {file.type !== "dir" && (
         <button
           onClick={() => downloadClickHandler()}
